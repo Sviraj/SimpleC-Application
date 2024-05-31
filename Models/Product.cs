3d971a95094b640072e9eae56b19b997c25bc697
sachinviraj <74185781+Sviraj@users.noreply.github.com>
2024-05-31 21:10:00 +0530

    Initial Commit

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Models
+{
+
+    public class Product()
+    {
+        public string name { get; set; } = string.Empty;
+        public string price { get; set; } = string.Empty;
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+using PasswordGenerator;
+using Newtonsoft.Json;
+using System;
+using Models;
+
+
+Console.WriteLine("Hello, Beautiful World!");
+var pwd = new Password();
+var password = pwd.Next();
+Console.WriteLine(password.ToString());
+Console.WriteLine(password);
+
+Product product = new Product();
+product.name = "Lux";
+product.price = "100";
+ string json = JsonConvert.SerializeObject(product);
+Console.WriteLine(json);
